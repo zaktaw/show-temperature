e7f7c7d7f6f5cce2de5f887fbc5211c0481f7f01
zaktaw <zaktaw@gmail.com>
2021-09-16 17:25:35 +0200

    Added function for getting weather data. Added a useState for loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+
 
 function App() {
 
-  function test() {
-    console.log("Test");
+  const [loading, setLoading] = useState(false);
+
+  async function getWeatherData() {
+
+    setLoading(true);
+
+    const requestOptions = {
+      method: 'GET'
+    };
+
+    const url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.9139&lon=10.7522';
+
+    const res = await fetch(url, requestOptions).then(setLoading(false));
+    const data = await res.json()
+    
+    // index 3 = this hour. Index 4 = next hour
+    let temperatureForNextHour = data.properties.timeseries[4].data.instant.details.air_temperature;
+    console.log(temperatureForNextHour);
   }
 
   return (
-    <Button variant="contained" onClick={test}>Hello World</Button>
+    <div>
+      { loading ? <h1>Loading...</h1> : <Button variant="contained" onClick={getWeatherData}>Show temperature</Button>}
+    </div>  
   );
 }
 
