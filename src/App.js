927f954047a8d2fe8142bfd5961479b93ea4208d
zaktaw <zaktaw@gmail.com>
2021-09-16 17:39:44 +0200

    Added useEffect for temperature. Temperature is now shown when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
-
+import LoopIcon from '@mui/icons-material/Loop';
 
 function App() {
 
+  const [temperature, setTemperatue] = useState(null);
   const [loading, setLoading] = useState(false);
 
   async function getWeatherData() {
@@ -22,12 +23,13 @@ function App() {
     
     // index 3 = this hour. Index 4 = next hour
     let temperatureForNextHour = data.properties.timeseries[4].data.instant.details.air_temperature;
-    console.log(temperatureForNextHour);
+    setTemperatue(temperatureForNextHour);
   }
 
   return (
     <div>
-      { loading ? <h1>Loading...</h1> : <Button variant="contained" onClick={getWeatherData}>Show temperature</Button>}
+      { loading ? <LoopIcon /> : <Button variant="contained" onClick={getWeatherData}>Show temperature</Button> }
+      { temperature ? <p>The temperature for the next hour is {temperature} degrees celsius.</p> : <p></p>}
     </div>  
   );
 }
